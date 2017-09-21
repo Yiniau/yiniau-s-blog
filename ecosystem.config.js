6688e27aa0ebed3909ab0fde8083cb8bee844665
yiniau <yiranzhouyida@gmail.com>
2017-09-21 23:21:35 +0800

    update ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'blog-api-server',
+      script    : './server/server.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+
+    // // Second application
+    // {
+    //   name      : 'WEB',
+    //   script    : 'web.js'
+    // }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'yiniau',
+      host : '45.77.16.113',
+      ref  : 'origin/master',
+      repo : 'git@github.com:Yiniau/yiniau-s-blog.git',
+      path : '/home/yiniau/yiniau-s-blog',
+      'post-deploy' : 'cd /home/yiniau/yiniau-s-blog/server && npm install && cd /home/yiniau/yiniau-s-blog && pm2 reload ecosystem.config.js --env production',
+      "env"  : {
+        "NODE_ENV": "production"
+      }
+    }
+  }
+};
