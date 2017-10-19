3f1bd25ff700fa7ba9c2c3168844e0a4e6e788f6
yiniau <yiranzhouyida@gmail.com>
2017-10-19 10:20:34 +0800

    路由修改

--- src/views/Blog/index.jsx
+++ src/views/Blog/index.jsx
@@ -174,7 +174,6 @@ class Blog extends Component {
       ];
     }
 
-
     return (
       <div className="Blog">
         <canvas className="fireworks">canvas 不可用，请使用最新的现代浏览器</canvas>
@@ -198,7 +197,7 @@ class Blog extends Component {
                   onClick={this.folderListEventHandler}>
                   <NavButton
                     key={`nb-${content}`}
-                    to={to}
+                    to={'/blog'}
                     icon={conf.icon}
                     content={content}
                     textStyle={textStyle}
@@ -252,8 +251,10 @@ class Blog extends Component {
         </SideBar>
         <MainContent>
           <Logo imgSrc={require('./avatar@90x90.jpg')} className='blog_mainContent_logo'/>
-          <Route exact strict path='/blog' component={BirdAnime}/>
-          <Route path='/blog/:folder/:title' component={Article}/>
+          <Switch>
+            <Route exact strict path='/blog' component={BirdAnime}/>
+            <Route path='/blog/:folder/:title' component={Article}/>
+          </Switch>
         </MainContent>
       </div>
     )
