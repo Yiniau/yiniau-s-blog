4376edfd3c265defae697719f43cd50f06c21671
yiniau <yiranzhouyida@gmail.com>
2017-12-26 18:48:45 +0800

    add cors allow origin

--- server/config/corsConfig.js
+++ server/config/corsConfig.js
@@ -4,6 +4,7 @@ const whiteList = process.env.NODE_ENV === 'production' ?
   [
     'https://yiniau.com',
     'https://test.yiniau.com',
+    'http://192.168.1.104:9999',
   ] : '*';
 
 module.exports = {
